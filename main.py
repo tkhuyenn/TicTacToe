eff83d3f41a40d05648d541f5f365eb60ce0fdf9
tkhuyenn <tkhuyen1212@gmail.com>
2021-11-11 23:58:14 +0700

    main

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import tkinter.messagebox
 from tkinter import *
 
 root = Tk()
-root.geometry("1350x750+0+0")  # 1,2: kích thước cửa sổ; 3,4: vị trí
+root.geometry("{0}x{1}+0+0".format(
+            root.winfo_screenwidth(), root.winfo_screenheight()))
+
 root.title("Tic Tac Toe")
 root.configure(background='lightcyan')
 
@@ -19,7 +21,7 @@ MainFrame.grid(row=1, column=0)
 LeftFrame = Frame(MainFrame, bd=10, width=750, height=500, pady=2, padx=10, bg='lightcyan', relief=RIDGE)
 LeftFrame.pack(side=LEFT)
 
-RightFrame = Frame(MainFrame, bd=10, width=560, height=500, pady=10, padx=2, bg='lightcyan', relief=RIDGE)
+RightFrame = Frame(MainFrame, bd=10, width=560, height=600, pady=10, padx=2, bg='lightcyan', relief=RIDGE)
 RightFrame.pack(side=RIGHT)
 
 RightFrame1 = Frame(RightFrame, bd=10, width=560, height=200, pady=10, padx=2, bg='lightcyan', relief=RIDGE)
@@ -277,44 +279,44 @@ txtPlayerO = Entry(RightFrame1, font=('arial', 40, 'bold'), bd=2, fg='black', te
 
 btnNewgame = Button(RightFrame2, text="New Game", font=('Times 26 bold'), height=3, width=20, bg='#5F9EA0',
                     command=NewGame)
-btnNewgame.grid(row=1, column=0, padx=6, pady=10)
+btnNewgame.grid(row=0, column=1, padx=6, pady=10)
 
 btnReset = Button(RightFrame2, text="Reset", font=('Times 26 bold'), height=3, width=20, bg='#5F9EA0', command=reset)
-btnReset.grid(row=0, column=0, padx=6, pady=10)
+btnReset.grid(row=0, column=2, padx=6, pady=10)
 
-button1 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=3, width=8, bg='#5F9EA0',
+button1 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=4, width=8, bg='#5F9EA0',
                  command=lambda: checker(button1))
 button1.grid(row=1, column=0, sticky=S + N + E + W)
 
-button2 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=3, width=8, bg='#5F9EA0',
+button2 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=4, width=8, bg='#5F9EA0',
                  command=lambda: checker(button2))
 button2.grid(row=1, column=1, sticky=S + N + E + W)
 
-button3 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=3, width=8, bg='#5F9EA0',
+button3 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=4, width=8, bg='#5F9EA0',
                  command=lambda: checker(button3))
 button3.grid(row=1, column=2, sticky=S + N + E + W)
 
-button4 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=3, width=8, bg='#5F9EA0',
+button4 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=4, width=8, bg='#5F9EA0',
                  command=lambda: checker(button4))
 button4.grid(row=2, column=0, sticky=S + N + E + W)
 
-button5 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=3, width=8, bg='#5F9EA0',
+button5 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=4, width=8, bg='#5F9EA0',
                  command=lambda: checker(button5))
 button5.grid(row=2, column=1, sticky=S + N + E + W)
 
-button6 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=3, width=8, bg='#5F9EA0',
+button6 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=4, width=8, bg='#5F9EA0',
                  command=lambda: checker(button6))
 button6.grid(row=2, column=2, sticky=S + N + E + W)
 
-button7 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=3, width=8, bg='#5F9EA0',
+button7 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=4, width=8, bg='#5F9EA0',
                  command=lambda: checker(button7))
 button7.grid(row=3, column=0, sticky=S + N + E + W)
 
-button8 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=3, width=8, bg='#5F9EA0',
+button8 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=4, width=8, bg='#5F9EA0',
                  command=lambda: checker(button8))
 button8.grid(row=3, column=1, sticky=S + N + E + W)
 
-button9 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=3, width=8, bg='#5F9EA0',
+button9 = Button(LeftFrame, text=" ", font=('Times 26 bold'), height=4, width=8, bg='#5F9EA0',
                  command=lambda: checker(button9))
 button9.grid(row=3, column=2, sticky=S + N + E + W)
 
